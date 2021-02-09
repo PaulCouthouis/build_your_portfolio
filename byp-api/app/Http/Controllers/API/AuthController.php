2a738ddd35ea0d1cc32c2b9fccfc2dfbdf0cfd36
Paul Couthouis <paulcouthouis@gmail.com>
2021-02-09 22:30:14 +0900

    feat: login WS

---
+++ byp-api/app/Http/Controllers/API/AuthController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class AuthController extends Controller
+{
+    public function login(Request $request)
+    {
+        $loginData = $request->validate([
+            'email' => 'email|required',
+            'password' => 'required'
+        ]);
+
+        if (!Auth::attempt($loginData)) {
+            return response(['message' => 'Invalid Credentials']);
+        }
+
+        $accessToken = Auth::user()->createToken('authToken')->accessToken;
+
+        return response(['user' => auth()->user(), 'access_token' => $accessToken]);
+    }
+}
