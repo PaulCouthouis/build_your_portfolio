6cf4a7256bc63556fcb6de2c63917ee4f24610c0
Paul Couthouis <paulcouthouis@gmail.com>
2021-02-13 12:44:35 +0900

    refacto: link is better

--- byp-bo/src/app/portfolios/portfolios.component.ts
+++ byp-bo/src/app/portfolios/portfolios.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
 import { select, Store } from '@ngrx/store';
+
 import { getPortfoliosAction } from '../store/actions';
 import { getPortfolios } from '../store/selectors';
 
@@ -13,13 +13,9 @@ export class PortfoliosComponent implements OnInit {
   readonly displayedColumns: string[] = ['name', 'edit'];
   readonly portfolios$ = this.store.pipe(select(getPortfolios));
 
-  constructor(private readonly store: Store, private readonly router: Router) {}
+  constructor(private readonly store: Store) {}
 
   ngOnInit(): void {
     this.store.dispatch(getPortfoliosAction());
-  }
-
-  navigateToNew(): void {
-    this.router.navigate(['/portfolio/new']);
   }
 }
